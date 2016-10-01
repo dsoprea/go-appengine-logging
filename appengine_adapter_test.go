f6e0ff3b4c63b01d4bf88f4f4aadd94f0c693d8f
Dustin Oprea <myselfasunder@gmail.com>
2016-10-01 14:28:21 -0400

    Moved general logging to separate project.
    - Added test.
    - Added Travis and Coveralls integrations.

---
+++ appengine_adapter_test.go
@@ -0,0 +1,32 @@
+package aelog
+
+import (
+    "testing"
+
+    "github.com/dsoprea/go-logging"
+
+    "golang.org/x/net/context"
+)
+
+func TestAppengine(t *testing.T) {
+    ecp := log.NewEnvironmentConfigurationProvider()
+    log.LoadConfiguration(ecp)
+
+    log.ClearAdapters()
+
+    aam := NewAppengineAdapterMaker()
+    log.AddAdapterMaker("appengine", aam)
+
+    an := log.GetDefaultAdapterName()
+    if an != "appengine" {
+        t.Error("Adapter was not properly registered.")
+    }
+
+    // Usually this is either useless or fundamentally broken in the context of
+    // AppEngine, but it works here and we won't otherwise have a useful 
+    // context since we're not running in response to a request.
+    ctx := context.Background()
+
+    l := log.NewLoggerWithAdapter("appengine_test", "appengine")
+    l.Debugf(ctx, "Test message.")
+}
